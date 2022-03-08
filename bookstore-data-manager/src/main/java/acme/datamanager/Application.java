c36ead5243c25c50f3985a3c738c78f7b3e37cc8
Ncoder2 <68146726+Ncoder2@users.noreply.github.com>
2022-03-08 09:13:52 +0530

    Update Application.java

--- bookstore-data-manager/src/main/java/acme/datamanager/Application.java
+++ bookstore-data-manager/src/main/java/acme/datamanager/Application.java
@@ -1,6 +1,7 @@
 package acme.datamanager;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
